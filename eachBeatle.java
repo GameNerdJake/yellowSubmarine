0116993697240d0a4c1d5958abe455d2e18d4b0f
jake <20567@my4county.com>
2021-12-15 11:37:15 -0500

    Added some sprites

--- eachBeatle.java
+++ eachBeatle.java
@@ -8,12 +8,95 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class eachBeatle extends Actor
 {
+    private int speed = 5;
+    private int vSpeed = 0;
+    private int acceleration = 1;
+    private int jumpStrength = 15;
+  
     /**
      * Act - do whatever the eachBeatle wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        checkKeys();
+        onGround();
+        checkFall();
+        respawn();
+        changeBeatle();
     }
+    
+     public boolean onGround()
+    {
+        Actor under = getOneObjectAtOffset(0, getImage().getHeight()/2, coral.class);
+        return under != null;
+    }
+    
+    public void checkKeys()
+    {
+        if (Greenfoot.isKeyDown("right"))
+        {
+            moveRight();
+        }
+        
+        if (Greenfoot.isKeyDown("left"))
+        {
+            moveLeft();
+        } 
+        if (Greenfoot.isKeyDown("space") && onGround())
+        {
+            jump();
+        }
+    }
+    
+    public void moveRight()
+    {
+        setLocation (getX() + speed, getY());
+    }
+    
+    public void moveLeft()
+    {
+        setLocation (getX() - speed, getY());
+    }
+    
+    public void fall()
+    {
+        setLocation (getX(), getY() + vSpeed);
+        vSpeed += acceleration;
+    }
+    
+     public void jump()
+    {
+        vSpeed = -jumpStrength;
+        fall();
+    }
+    
+    public void checkFall()
+    {
+        if(onGround())
+        {
+            vSpeed = 0;
+        }
+        else
+        {
+            fall();
+        }
+    }
+    
+    public void respawn()
+    {
+        if (getY() == 799){
+            setLocation(133, 700);
+            vSpeed = 0;
+        }       
+    }
+    
+    public void changeBeatle()
+    {
+        if (isTouching (yellowSubmarine.class)){
+            setImage("paul.png");
+        }
+    }
+
 }
+
--- underWater.java
+++ underWater.java
@@ -17,5 +17,24 @@ public class underWater extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 800, 1); 
+        prepare();
+    }
+    
+    private void prepare()
+    {
+        yellowSubmarine yellowSubmarine = new yellowSubmarine();
+        addObject(yellowSubmarine, 300, 120);
+        
+        coral coral1 = new coral();
+        addObject(coral1, 75, 790);
+        
+        coral coral2 = new coral();
+        addObject(coral2, 225, 790);
+        
+        groupBeatles group = new groupBeatles();
+        addObject(group, 55, 732);
+        
+        eachBeatle single = new eachBeatle();
+        addObject(single, 133, 741);
     }
 }
